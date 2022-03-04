9078c2b01b7a2ee9b202b714583961a3c625cac7
Tonmoy Jitu <tonmoy@192-168-1-2.tpgi.com.au>
2022-03-04 19:14:32 +1100

    js bug fix, navbar functionality fixed on smaller screens

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const soundcloud = document.querySelector('.sound-cloud');
+const soundCloud = document.querySelector('.sound-cloud');
 const off = document.querySelector('#off');
 const on = document.querySelector('#on');
 const myAudio = document.querySelector('#myAudio');
 
-// adding a listerner for the on and off button
 off.addEventListener('click', () => soundTrack('off'));
-on.addEventListener('clik', () => soundTrack('on'));
+on.addEventListener('click', () => soundTrack('on'));
 
-// fucntion for soundtrack, transitions button from on/off
 const soundTrack = (soundState) => {
-    if(soundState === 'off') {
+    if(soundState === 'off'){
         on.style.display = 'block';
         off.style.display = 'none';
-        soundcloud.style.color = "#08fdd8";
+        soundCloud.style.color = "#08fdd8";
         myAudio.play();
     }
-    else if(soundState === 'on') {
+    else if(soundState === 'on'){
         on.style.display = 'none';
         off.style.display = 'block';
-        soundcloud.style.color = "#f50057";
+        soundCloud.style.color = "#f50057";
         myAudio.pause();
     }
 }
 
-// function to play/pause and display nav bar
+// Play music functionality
+
 const btnBars = document.querySelector('.bars');
-const btnTimes = document.querySelector('times');
-const sideNav = document.querySelector('.aside')
+const btnTimes = document.querySelector('.times');
+const SideNav = document.querySelector('.aside');
+
 
 btnBars.addEventListener('click', () => myFunc('open'));
-btnBars.addEventListener('click', () => myFunc('close'));
+btnTimes.addEventListener('click', () => myFunc('close'));
 
 const myFunc = (navCondition) => {
-    if(navCondition === 'open') {
-        sideNav.classList.add('show-nav')
+    if(navCondition === 'open'){
+        SideNav.classList.add('show-nav');
         btnTimes.style.display = "block";
         btnBars.style.display = "none";
     }
-    else if(navCondition === 'close') {
-        sideNav.classList.remove('show-nav')
-        btnTimes.style.display = "none";
-        btnBars.style.display = "block";
+    else if(navCondition === 'close'){
+            SideNav.classList.remove('show-nav');
+            btnTimes.style.display = "none";
+            btnBars.style.display = "block";
     }
-
 }
+
